574b798f2ce5beb91363f75889849afd104dc6bf
quyvu <quy.vu.0101@gmail.com>
2025-03-19 11:30:54 +0700

    Update Code, request

--- src/FlowX/Implementations/FlowPipelinesImpl.cs
+++ src/FlowX/Implementations/FlowPipelinesImpl.cs
@@ -7,7 +7,7 @@ public class FlowPipelinesImpl<TRequest, TResult>(
     IRequestHandler<TRequest, TResult> handler)
     where TRequest : IRequest<TResult>
 {
-    public Task<TResult> ExecuteAsync(RequestContext<TRequest> requestContext) =>
+    public Task<TResult> ExecuteAsync(IRequestContext<TRequest> requestContext) =>
         behaviors.Reverse().Aggregate(() => handler.HandleAsync(requestContext),
             (acc, pipeline) => () => pipeline.HandleAsync(requestContext, acc)).Invoke();
 }
